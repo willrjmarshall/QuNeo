be2107782aae1eb4398bf6c81eb9da68107bd346
Will Marshall <will@willmarshall.me>
2013-04-30 13:26:50 -0700

    First commit, working shift button

---
+++ QuNeo.py
@@ -0,0 +1,24 @@
+from __future__ import with_statement
+
+import Live
+import time
+import math
+
+from _Framework.ControlSurface import ControlSurface
+from QuNeoSelectorComponent import QuNeoSelectorComponent
+from MIDI_Map import *
+
+
+class QuNeo(ControlSurface):
+  """ Root class for the KMI QuNeo """
+
+  def __init__(self, c_instance):
+    ControlSurface.__init__(self, c_instance)
+    with self.component_guard():
+      self.setup_selector()
+      #self.setup_mixer()
+      #self.setup_transport()
+
+
+  def setup_selector(self):
+    self._selector = QuNeoSelectorComponent(CLIP_NOTE_MAP, SHIFT_BUTTON)
---
+++ QuNeoSelectorComponent.py
@@ -0,0 +1,46 @@
+from _Framework.ModeSelectorComponent import ModeSelectorComponent
+from _Framework.ButtonElement import ButtonElement
+from _Framework.InputControlElement import *
+from MIDI_Map import *
+
+class QuNeoSelectorComponent(ModeSelectorComponent):
+  """ Class that reassigns the QuNeo Matrix between session control and step sequencer modes """
+
+  def __init__(self, matrix, shift_button): 
+    ModeSelectorComponent.__init__(self)
+    self.shift_button = None
+    #self.setup_session(matrix)
+    self.setup_shift(shift_button)
+    #self.setup_buttons(mode_buttons)
+
+  def setup_shift(self, shift_button):
+    shift_button = ButtonElement(True, MIDI_NOTE_TYPE, PAD_CHANNEL, shift_button) 
+    if (self.shift_button != shift_button):
+      if (self.shift_button != None):
+          self.shift_button.remove_value_listener(self._shift_value)
+      self.shift_button = shift_button
+      if (self.shift_button != None):
+          self.shift_button.add_value_listener(self._shift_value)
+
+  def _shift_value(self, value):
+    if value > 0:
+      self.shift_on()
+    else:
+      self.shift_off()
+
+  def shift_on(self):
+    self.shift_button.turn_on()
+
+  def shift_off(self):
+    self.shift_button.turn_off()
+
+  def setup_buttons(mode_buttons):
+    self.set_mode_buttons([self.button(note) for note in mode_buttons])
+    #self._directional_buttons = [self.button(note) for note in directional_buttons]
+    
+
+  def number_of_modes(self):
+    return 2
+
+  def update(self):
+    pass
